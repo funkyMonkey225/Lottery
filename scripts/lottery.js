62f4da9d29fea568b9bcc16292fdef98a2e0f685
Mac User <sabbey37@gmail.com>
2017-07-19 10:42:20 -0400

    Create addListener function

--- scripts/lottery.js
+++ scripts/lottery.js
@@ -1,2 +1,14 @@
-var DISPLAY_SELECTOR = '[data-role="display"]'
+var DISPLAY_SELECTOR = '[data-role="display"]';
+var GENERATOR_SELECTOR = '[data-role="generator"]';
+var generator = querySelector(GENERATOR_SELECTOR);
 
+
+
+
+
+function addListener(element) {
+    element.addEventListener("click", function(event) {
+        event.preventDefault();
+
+    })
+}
