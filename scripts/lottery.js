b1ea74453817c6193abf814a2eb07358bf011b09
Mac User <sabbey37@gmail.com>
2017-07-19 10:50:25 -0400

    Add range function, random function

--- scripts/lottery.js
+++ scripts/lottery.js
@@ -3,6 +3,25 @@ var GENERATOR_SELECTOR = '[data-role="generator"]';
 var generator = querySelector(GENERATOR_SELECTOR);
 
 
+function range(min, max) {
+  var arr = [];
+  for (var i = min; i < max; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
+
+function randomNums(num) {
+    
+}
+
+function generateNumsArray() {
+    var range1 = range(0, 4);
+    var range1.map();
+    
+    num = Math.floor(Math.random() * 4)
+}
+
 
 
 
